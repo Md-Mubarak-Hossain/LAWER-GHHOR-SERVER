58d977cf956e838e22483d75ca41002903123f58
Md-Mubarak-Hossain <hossainmubarak477@gmail.com>
2022-11-08 19:02:51 +0600

    blog and faq data read from database

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+// const blog = require('./blog.json');
 const app = express()
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 // middle wares
 app.use(cors())
@@ -12,17 +14,26 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster01.encrv7o.mongodb.net/?retryWrites=true&w=majority`;
 
+console.log(uri);
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 async function run() {
     try {
         // database and collection create
         const database = client.db('lawyerServer')
         const collectionServer = database.collection('services')
+        const faqCollection = database.collection('faq')
+        const blogCollection = database.collection('blog')
 
         // data create and send
         app.post('/services', async (req, res) => {
-            const user = req.body;
+            // const user = req.body;
+            const user = {
+                name: "Rashed",
+                email: 'rashed@gmail.com'
+            };
             const result = await collectionServer.insertOne(user);
             res.send(result);
             console.log(result);
@@ -34,6 +45,20 @@ async function run() {
             const cursor = collectionServer.find(query)
             const users = await cursor.toArray();
             res.send(users);
+        })
+        // blog data read
+        app.get('/blog', async (req, res) => {
+            const query = {}
+            const cursor = blogCollection.find(query)
+            const blog = await cursor.toArray();
+            res.send(blog);
+        })
+        // faq data read
+        app.get('/faq', async (req, res) => {
+            const query = {}
+            const cursor = faqCollection.find(query)
+            const faq = await cursor.toArray();
+            res.send(faq);
         })
 
         //single data read with id and view
@@ -76,6 +101,10 @@ async function run() {
 }
 run().catch(err => console.error(err))
 
+// data read test
+// app.get('/blog', (req, res) => {
+// res.send(blog);
+// })
 app.get('/', (req, res) => {
     res.send('server-mongo-connect running')
 })
